dac66ca0816733fb912b6c2162627bf806f508cc
Mejsun <88291174+Mejsun@users.noreply.github.com>
2021-10-26 15:00:33 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const mainScreen = document.querySelector('.pokeData')
 const grid = document.querySelector('.grid')
+const cell = document.querySelectorAll('.cell')
 const pokeID = document.querySelector('.pokeId')
 const pokeName = document.querySelector('.pokeName')
 const pokeImageFront = document.querySelector('.pokeImageFront')
@@ -13,9 +14,28 @@ const pokemonListItem = document.querySelectorAll('.pokemonItemList')
 const prevBtn = document.querySelector('.left')
 const nextBtn = document.querySelector('.right')
 const searchBar = document.querySelector('.searchBar')
+const colours = {
+    fire: 'rgb(255, 194, 173)',
+    grass: 'rgb(203, 245, 174)',
+    electric: 'yellow',
+    water: 'rgb(174, 245, 245)',
+    ground: 'rgb(209, 191, 171)',
+    rock: 'rgb(181, 178, 174)',
+    fairy: 'rgb(245, 213, 237)',
+    poison:'rgb(211, 172, 230)',
+    bug: 'turquoise',
+    dragon:'navy',
+    psychic: 'gold',
+    flying: 'skyblue',
+    fighting: 'rgb(245, 76, 87)',
+    normal: 'white'
+};
 
 const capitalise = (string) => string[0].toUpperCase() + string.substring(1);
 
+const resetScreen = () => {
+    grid.classList.add('hide');
+}
 const pokeListFetched = (url) => {
 fetch (url)
     .then (res => res.json())
@@ -32,28 +52,29 @@ fetch (url)
         }else{
             pokemonListItems.textContent = ''
         }
-
+        
         pokemonListItems.addEventListener('click', (e) => {
             const listItem = e.target;
             let id = listItem.textContent.split('.')[0];
             pokeIdFetched(id);
+            
         })
         
     }
     })
 }
 
-
 const pokeIdFetched = id => {
 fetch (`https://pokeapi.co/api/v2/pokemon/${id}`)
     .then (res => res.json())
     .then (data => {
-        
+            
         pokeID.textContent = data['id'].toString().padStart(3,'0');
         pokeName.textContent = capitalise(data['name']);
         pokeHeight.textContent = 'Height: ' + data['height']
         pokeWeight.textContent = 'Weight: ' + data['weight']
-        pokeTypeOne.textContent = 'Type 1: ' +capitalise(data['types'][0]['type']['name']);
+        const dataTypeOne = data['types'][0]['type']['name']
+        pokeTypeOne.textContent = 'Type 1: ' +capitalise(dataTypeOne);
         const dataTypeTwo = data['types'][1];
         if(dataTypeTwo){
             pokeTypeTwo.textContent = 'Type 2: ' + capitalise(dataTypeTwo['type']['name']);
@@ -61,47 +82,50 @@ fetch (`https://pokeapi.co/api/v2/pokemon/${id}`)
             pokeTypeTwo.textContent = '';
             pokeTypeTwo.style.display = 'none'
         }
+        
+        const colour = colours[dataTypeOne]
+        grid.style.backgroundColor = colour;
+        
         pokeImageFront.style.display = 'block';
         pokeImageBack.style.display = 'block'
         pokeImageFront.src = data['sprites']['front_default'] || '';
         pokeImageBack.src = data['sprites']['back_default'] || ''
 
-        prevBtn.addEventListener('click', (e) => {
+
+        for (let i = id; i <= pokemonListItem.length; i++){
             let i = parseInt(id);
             let prevPoke = i-1;
-            console.log(prevPoke)
-            if (prevPoke !==0) {
-                return prevPoke;   
-            }else{
-                prevPoke.disabled = true
-            }
-        })
-
-        nextBtn.addEventListener('click', (e) => {
-            let i = parseInt(id);
             let nextPoke = i+1;
-            console.log(nextPoke)
-            if (nextPoke) {
-                return nextPoke;
-            }else{
-                nextPoke.disabled = true;
-            }
-        })
 
+            function prevBtnClicked (){
+                console.log(prevPoke)
+                if (prevPoke !==0) {
+                    return prevPoke;   
+                }else{
+                    prevPoke.disabled = true
+                }}
+        
+            function nextBtnClicked () {
+                console.log(nextPoke)
+                if (nextPoke) {
+                    return nextPoke;
+                }else{
+                    nextPoke.disabled = true;
+                }}
+
+            resetScreen();
+
+            prevBtn.addEventListener('click', prevBtnClicked);
+            nextBtn.addEventListener('click', nextBtnClicked);
+        }
     })
+
 }
 
 pokeListFetched('https://pokeapi.co/api/v2/pokemon?offset=0&limit=30');
 
 
 /*  
-
-for (i = 0; i < pokemonListItem.length; i++){
-        let i = parseInt(id);
-        let prevPoke = i-1;
-        let nextPoke = i+1;
-
-        console.log(prevPoke, nextPoke)
 
         nextBtn.onclick = () => {
             if (nextPoke) {
@@ -125,43 +149,6 @@ for (i = 0; i < pokemonListItem.length; i++){
 
     }
 
-
-
-nextBtn.addEventListener('click', () => {
-            if (nextPoke) {
-                return nextPoke;
-            }
-        });
-
-        prevBtn.addEventListener('click', () => {
-            if (prevPoke !== 0) {
-                return prevPoke;  
-            }  
-        });
-
-
-
-for (let i = 0; i < pokemonListItem.length; i++){
-    let i = parseInt(pokeIdFetched);
-    let prevPoke = i-1;
-    let nextPoke = i+1;
-
-
-    let nextBtnClick = (e) => {
-        if (nextPoke === pokemonListItem.length){
-            return 0;
-        }else{
-            return nextPoke;
-        }
-    }
-
-    let prevBtnClick = (e) => {
-        if (prevPoke === 0) {
-            return pokemonListItem.length - 1;
-        }else {
-            return prevPoke;   
-        }
-    }
 
     nextBtn.addEventListener('click', nextBtnClick);
     prevBtn.addEventListener('click', prevBtnClick);
@@ -217,5 +204,9 @@ for (let i = 0; i < pokemonListItem.length; i++) {
             if (prevPoke !== 0){
                 pokeIdFetched (prevPoke);
         }}}
-*/
+
+
+reset screen, add pokelist, search bar, prev and next btns
+
+        */
 
